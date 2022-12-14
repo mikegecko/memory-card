c095effe5a44d7796845b76aff9f49010a390f2c
Michael Guedko <mjgeko@gmail.com>
2022-12-14 16:15:21 -0500

    Scoreboard displays scores from state

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,13 +1,13 @@
 
 
-function Scoreboard () {
+function Scoreboard (props) {
 
     return(
         <div className="header frosted">
             <h1 className="title font-face-gen">Genshin Memory</h1>
             <div className="score">
-                <div>Score:</div>
-                <div>Highscore:</div>
+                <div>Score: {props.score}</div>
+                <div>Highscore: {props.best}</div>
             </div>
         </div>
     );
