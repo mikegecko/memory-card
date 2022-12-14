a69c612b76a7e0f7a59d22b775854ee3b84f6908
Michael Guedko <mjgeko@gmail.com>
2022-12-14 13:49:33 -0500

    Initial commit
        -Has some basic elements and styling

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,16 @@
+
+
+function Scoreboard () {
+
+    return(
+        <div className="header frosted">
+            <h1 className="title font-face-gen">Genshin Memory</h1>
+            <div className="score">
+                <div>Score:</div>
+                <div>Highscore:</div>
+            </div>
+        </div>
+    );
+}
+
+export default Scoreboard;
