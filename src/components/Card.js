ed1de4bc47f48e455d31bb0089dd7af6a828aac9
Michael Guedko <mjgeko@gmail.com>
2022-12-14 18:41:26 -0500

    Add tilt component to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,15 @@
+import Tilt from "react-parallax-tilt";
 
-function Card (props) {
-    //Gotta add eventhandlers here i think
-    return(
-        <div className="card frosted font-face-gen">
-            <img className="char" src={props.card.img} alt={props.card.name}/>
-            <p>{props.card.name}</p>
-        </div>
-    );
+function Card(props) {
+  //Gotta add eventhandlers here i think
+  return (
+    <Tilt glareEnable={true} glareBorderRadius="16px" glareMaxOpacity={0.6} glarePosition='all' scale={1.1}>
+      <div className="card frosted font-face-gen data-tilt">
+        <img className="char" src={props.card.img} alt={props.card.name} />
+        <p>{props.card.name}</p>
+      </div>
+    </Tilt>
+  );
 }
 
 export default Card;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,22 +1,19 @@
 import Card from "./Card";
 
-
-function Game (props) {
-
-    return (
-        <div className="container">
-            {props.cards.map((el,index) => {
-                if(index < 12){
-                    return(
-                        <div key={index} className="card-container">
-                            <Card card={el} />
-                        </div>
-                    )
-                }
-                return;
-                })
-            }
-        </div>
-    );
+function Game(props) {
+  return (
+    <div className="container">
+      {props.cards.map((el, index) => {
+        if (index < 12) {
+          return (
+            <div key={index} className="card-container">
+              <Card card={el} />
+            </div>
+          );
+        }
+        return;
+      })}
+    </div>
+  );
 }
 export default Game;
